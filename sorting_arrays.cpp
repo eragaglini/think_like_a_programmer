2c860dc0f5eabb848b8210701c2b35eab69ec9ec
edoardo.ragaglini <eragaglini7@gmail.com>
2025-04-21 11:01:16 +0200

    added exercise 3-7

--- sorting_arrays.cpp
+++ sorting_arrays.cpp
@@ -112,27 +112,79 @@ void getCipherText(const char charArray1[], char result[], int textLength, const
     }
 }
 
+void removeNumber(int numbers[], int idx, int &size)
+{
+    int i;
+    for (i = idx; i < size - 1; i++)
+        numbers[i] = numbers[i + 1];
+    size--;
+}
+
+// Function to remove duplicate  
+// elements This function returns  
+// new size of modified array. 
+int removeDuplicates(int arr[], int n) 
+{ 
+    // Return, if array is empty or  
+    // contains a single element 
+    if (n == 0 || n == 1) 
+        return n; 
+  
+    int temp[n]; 
+  
+    // Start traversing elements 
+    int j = 0; 
+  
+    // If current element is not equal  
+    // to next element then store that  
+    // current element 
+    for (int i = 0; i < n - 1; i++) 
+        if (arr[i] != arr[i + 1]) 
+            temp[j++] = arr[i]; 
+  
+    // Store the last element as whether  
+    // it is unique or repeated, it hasn't  
+    // stored previously 
+    temp[j++] = arr[n - 1]; 
+  
+    // Modify original array 
+    for (int i = 0; i < j; i++) 
+        arr[i] = temp[i]; 
+  
+    return j; 
+} 
+
+void printArray(const int intArray[], int size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        /* code */
+        cout << intArray[i] << " ";
+    }
+    cout << endl;
+}
+
+
 int main()
 {
-    // sort(students, students + STUDENTS_SIZE, compareByGrade);
+    sort(students, students + STUDENTS_SIZE, compareByGrade);
 
-    // for (size_t i = 0; i < STUDENTS_SIZE; i++)
-    // {
-    //     /* code */
-    //     cout << "Name: " << students[i].name << ", grade: " << students[i].grade << endl;
-    // }
+    for (size_t i = 0; i < STUDENTS_SIZE; i++)
+    {
+        cout << "Name: " << students[i].name << ", grade: " << students[i].grade << endl;
+    }
 
-    // string name;
-    // cout << "Enter your name: ";
+    string name;
+    cout << "Enter your name: ";
 
     // Taking string as input
-    // getline(cin, name);
+    getline(cin, name);
 
-    // int index = searchByName(students, STUDENTS_SIZE, name);
-    // if (index >= 0)
-    //     cout << "Grade: " << students[index].grade << endl;
-    // else
-    //     cout << "No student found" << endl;
+    int index = searchByName(students, STUDENTS_SIZE, name);
+    if (index >= 0)
+        cout << "Your Grade: " << students[index].grade << endl;
+    else
+        cout << "No student found for the given name." << endl;
 
     const int NUM_AGENTS = 3;
     const int NUM_MONTHS = 12;
@@ -184,12 +236,11 @@ int main()
 
     cout << checkIfSorted(unsortedArray3, 15) << endl;
 
-
+    // creatin a cipher alphabet by copying and shuffling the alphabet
     char cipherAlphabet[ALPHABET_LENGHT];
     strncpy(cipherAlphabet, alphabet, 26);
     random_shuffle(&cipherAlphabet[0], &cipherAlphabet[25]);
     cout << cipherAlphabet << endl;
-
 
     char plainText[] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG, PACKING MY BOX WITH FIVE DOZEN LIQUOR JUGS.";
     int textLength = sizeof(plainText) / sizeof(char);
@@ -210,8 +261,57 @@ int main()
     cout << endl;
     cout << "Ciphertext: " << endl;
     cout << cipherText << endl;
-    cout << "Plain Text: " << endl;
+    cout << "Decoded Message: " << endl;
     cout << decodedMessage << endl;
+
+    // given an array of integers and determines the mode,
+    // which is the number that appears most frequently in the array.
+    const int numbers[] = {14, 14, 27, 27, 5, 5, 5, 18, 18, 31, 31, 9, 9, 22, 22, 1, 1, 16, 16, 25, 25, 11, 11, 7, 7, 20, 20, 4, 4, 29};
+    int numbers_length = sizeof(numbers) / sizeof(int);
+    int sorted_numbers[numbers_length];
+    for (size_t i = 0; i < numbers_length; i++)
+    {
+        /* code */
+        sorted_numbers[i] = numbers[i];
+    }
+    sort(sorted_numbers, sorted_numbers + numbers_length);
+    printArray(sorted_numbers, numbers_length);
+
+    int unique_length = removeDuplicates(sorted_numbers, numbers_length);
+    int array_count[unique_length];
+    printArray(sorted_numbers, numbers_length);
+
+    for (size_t i = 0; i < unique_length; i++)
+    {
+        /* code */
+        int num = sorted_numbers[i];
+        int count = 0;
+        for (size_t j = 0; j < numbers_length; j++)
+        {
+            /* code */
+            if (numbers[j] ==  num)
+            {
+                /* code */
+                count ++;
+            }
+        }
+        array_count[i] = count;
+    }
+
+    int max = array_count[0];
+    int max_index = 0;
+    for (size_t i = 1; i < unique_length; i++)
+    {
+        /* code */
+        if (array_count[i] > max)
+        {
+            /* code */
+            max = array_count[i];
+            max_index = i;
+        }
+    }
+    cout << "the number that appears most frequently in the array is " << sorted_numbers[max_index] << " with: " << max << " occurences." << endl;
+    
 
     return 0;
 }
