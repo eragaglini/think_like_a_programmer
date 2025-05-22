eac332bfe8e3c35b60c8b0001e25de87174a7a75
edoardo.ragaglini <eragaglini7@gmail.com>
2025-05-22 13:53:05 +0200

    added exercise 7-3

--- students_with_stl_classes.cpp
+++ students_with_stl_classes.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <bits/stdc++.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,6 +12,11 @@ struct studentRecord
 };
 typedef std::vector<studentRecord> studentCollection;
 
+bool compare_by_id(const studentRecord &a, const studentRecord &b)
+{
+    return a.studentNum < b.studentNum;
+}
+
 void addRecord(studentCollection &sc, int stuNum, int gr)
 {
     studentRecord record = {
@@ -18,6 +24,7 @@ void addRecord(studentCollection &sc, int stuNum, int gr)
         gr      // grade
     };
     sc.push_back(record);
+    sort(sc.begin(), sc.end(), compare_by_id);
 }
 
 double averageRecord(studentCollection sc)
@@ -44,12 +51,53 @@ void removeRecord(studentCollection &sc, int stuNum)
         sc.end());
 }
 
+int interpolation_search(studentCollection &sc, int lo, int hi, int stuNum)
+{
+    int pos;
+
+    // Since array is sorted, an element present
+    // in array must be in range defined by corner
+    if (lo <= hi && (stuNum >= sc[lo].studentNum) && (stuNum <= sc[hi].studentNum))
+    {
+
+        // Probing the position with keeping
+        // uniform distribution in mind.
+        pos = lo + (((stuNum - sc[lo].studentNum) * (hi - lo)) / (sc[hi].studentNum - sc[lo].studentNum));
+        // Condition of target found
+        if (sc[pos].studentNum == stuNum)
+            return pos;
+
+        // If x is larger, x is in right sub array
+        if (sc[pos].studentNum < stuNum)
+            return interpolation_search(sc, pos + 1, hi, stuNum);
+
+        // If x is smaller, x is in left sub array
+        if (sc[pos].studentNum > stuNum)
+            return interpolation_search(sc, lo, pos - 1, stuNum);
+    }
+    return -1;
+}
+
 int main()
 {
     studentCollection sc;
-    addRecord(sc, 1274, 91);
-    addRecord(sc, 1265, 118);
-    addRecord(sc, 1119, 60);
+    // Uniformly distributed student IDs, step = 10
+    addRecord(sc, 1000, 75);
+    addRecord(sc, 1010, 82);
+    addRecord(sc, 1020, 91);
+    addRecord(sc, 1030, 67);
+    addRecord(sc, 1040, 88);
+    addRecord(sc, 1050, 60);
+    addRecord(sc, 1060, 95);
+    addRecord(sc, 1070, 73);
+    addRecord(sc, 1080, 99);
+    addRecord(sc, 1090, 54);
+    addRecord(sc, 1100, 77);
+    addRecord(sc, 1110, 85);
+    addRecord(sc, 1120, 92);
+    addRecord(sc, 1130, 61);
+    addRecord(sc, 1140, 78);
+
     cout << "Students: " << endl;
     printStudentCollection(sc);
     cout << " ------------ " << endl;
@@ -62,5 +110,16 @@ int main()
     cout << "Students after trying to remove student with id == 1001: " << endl;
     printStudentCollection(sc);
     cout << " ------------ " << endl;
+
+    // student id to be searched
+    int stuNum = 1070;
+    int index = interpolation_search(sc, 0, sc.size() -1, stuNum);
+
+    // If element was found
+    if (index != -1)
+        cout << "Element found at index " << index << endl;
+    else
+        cout << "Element not found." << endl;
+
     return 0;
 }
