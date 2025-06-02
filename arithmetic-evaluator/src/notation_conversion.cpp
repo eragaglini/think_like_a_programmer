bb9fe02924ffd9d5cf9170af0e1489dc3fb1bf02
eragaglini <eragaglini7@gmail.com>
2025-06-02 15:02:04 +0200

    improved notation conversion to handle parenthesis

--- arithmetic-evaluator/src/notation_conversion.cpp
+++ arithmetic-evaluator/src/notation_conversion.cpp
@@ -15,8 +15,20 @@ int get_precedence(char ch)
         return 0;
 }
 
-vector<token> infix_to_postfix_helper(vector<token>& tokens, stack<token>& s,
-                                      vector<token>::iterator& it)
+int find_starting_point(const vector<token>& tokens)
+{
+    for (int i = tokens.size() - 1; i >= 0; --i)
+    {
+        if (tokens[i].c == '(')
+        {
+            return i + 1;
+        }
+    }
+    return 0;
+}
+
+void infix_to_postfix_helper(vector<token>& tokens, stack<token>& s,
+                             vector<token>::iterator& it)
 {
     vector<token> postfix_tokens;
     vector<token> operators_buffer;
@@ -36,46 +48,85 @@ vector<token> infix_to_postfix_helper(vector<token>& tokens, stack<token>& s,
             {
                 operators_buffer.push_back(t);
             }
-            else if (get_precedence(operators_buffer[0].c) == precedence)
-            {
-                s.push(operators_buffer[0]);
-                operators_buffer[0] = t;
-            }
-            else if (precedence <= get_precedence(operators_buffer.back().c))
-            {
-                for (size_t i = 0; i < operators_buffer.size(); i++)
-                {
-                    s.push(operators_buffer[i]);
-                }
-                operators_buffer.clear();
-                operators_buffer.push_back(t);
-            }
             else
             {
-                auto pos = std::find_if(operators_buffer.begin(),
-                                        operators_buffer.end(),
-                                        [t](auto s) { return s < t; });
-                operators_buffer.insert(pos, t);
+                int index = 0;
+                index = find_starting_point(operators_buffer);
+                cout << "starting point: " << index << endl;
+                if (get_precedence(operators_buffer[index].c) == precedence)
+                {
+                    s.push(operators_buffer[index]);
+                    operators_buffer[index] = t;
+                }
+                else if (precedence <=
+                         get_precedence(operators_buffer.back().c))
+                {
+                    for (size_t i = index; i < operators_buffer.size(); i++)
+                    {
+                        s.push(operators_buffer[i]);
+                    }
+                    operators_buffer.clear();
+                    operators_buffer.push_back(t);
+                }
+                else
+                {
+                    auto pos = std::find_if(operators_buffer.begin() + index,
+                                            operators_buffer.end(),
+                                            [t](auto s) { return s < t; });
+                    operators_buffer.insert(pos, t);
+                }
             }
         }
+        else if (t.c == '(')
+        {
+            // Treat as marker for future closing parenthesis
+            operators_buffer.push_back(t);
+        }
+        else if (t.c == ')')
+        {
+            // Pop until matching '(' is found
+            while (operators_buffer.back().c != '(')
+            {
+                s.push(operators_buffer.back());
+                operators_buffer.pop_back();
+            }
+
+            // If operators_buffer is empty, mismatched parenthesis
+            if (operators_buffer.empty())
+            {
+                throw std::invalid_argument("Mismatched closing parenthesis");
+            }
+
+            // Discard the opening parenthesis '('
+            operators_buffer.pop_back();
+        }
+
+        cout << "Operators: [";
+        for (size_t i = 0; i < operators_buffer.size(); i++)
+        {
+            /* code */
+            cout << operators_buffer[i].c << ", ";
+        }
+        cout << "]" << endl;
         ++it;
     }
+
     for (auto it = operators_buffer.begin(); it != operators_buffer.end(); ++it)
     {
         s.push(*it);
     }
-
-    while (!s.empty())
-    {
-        postfix_tokens.push_back(s.top());
-        s.pop();
-    }
-    return postfix_tokens;
 }
 
 vector<token> infix_to_postfix(vector<token>& tokens)
 {
     stack<token> s;
     vector<token>::iterator it = begin(tokens);
-    return infix_to_postfix_helper(tokens, s, it);
+    vector<token> posftix_tokens;
+    infix_to_postfix_helper(tokens, s, it);
+    while (!s.empty())
+    {
+        posftix_tokens.push_back(s.top());
+        s.pop();
+    }
+    return posftix_tokens;
 }
--- arithmetic-evaluator/tests/notation_conversion_test.cpp
+++ arithmetic-evaluator/tests/notation_conversion_test.cpp
@@ -30,12 +30,40 @@ TEST(InfixToPostfixTest, MultipleOperators) {
   EXPECT_EQ(expected_postfix, actual_postfix);
 }
 
-// TEST(InfixToPostfixTest, Parentheses) {
-//   std::vector<token> infix = tokenize_expression("(2+3)*4-5");
-//   std::string expected_postfix = "23+4*5-";
-//   std::string actual_postfix = token_vector_to_string(infix_to_postfix(infix));
-//   EXPECT_EQ(expected_postfix, actual_postfix);
-// }
+TEST(InfixToPostfixTest, Parentheses) {
+  std::vector<token> infix = tokenize_expression("(2+3)*4-5");
+  std::string expected_postfix = "23+4*5-";
+  std::string actual_postfix = token_vector_to_string(infix_to_postfix(infix));
+  EXPECT_EQ(expected_postfix, actual_postfix);
+}
+
+TEST(InfixToPostfixTest, NestedParentheses) {
+  std::vector<token> infix = tokenize_expression("(2+(3*4))*5");
+  std::string expected_postfix = "234*+5*";
+  std::string actual_postfix = token_vector_to_string(infix_to_postfix(infix));
+  EXPECT_EQ(expected_postfix, actual_postfix);
+}
+
+TEST(InfixToPostfixTest, ParenthesesWithMultipleOperators) {
+  std::vector<token> infix = tokenize_expression("(2+3)*(4-5)+6");
+  std::string expected_postfix = "23+45-*6+";
+  std::string actual_postfix = token_vector_to_string(infix_to_postfix(infix));
+  EXPECT_EQ(expected_postfix, actual_postfix);
+}
+
+TEST(InfixToPostfixTest, ParenthesesAtStartAndEnd) {
+  std::vector<token> infix = tokenize_expression("((2+3)*4)");
+  std::string expected_postfix = "23+4*";
+  std::string actual_postfix = token_vector_to_string(infix_to_postfix(infix));
+  EXPECT_EQ(expected_postfix, actual_postfix);
+}
+
+TEST(InfixToPostfixTest, MultipleParentheses) {
+  std::vector<token> infix = tokenize_expression("(2+3)*(4+5)-((6-7)*8)");
+  std::string expected_postfix = "23+45+*67-8*-";
+  std::string actual_postfix = token_vector_to_string(infix_to_postfix(infix));
+  EXPECT_EQ(expected_postfix, actual_postfix);
+}
 
 
 TEST(InfixToPostfixTest, SingleNumber)
