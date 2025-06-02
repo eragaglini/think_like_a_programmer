935d577b453133e5abd2c318f8a9567926adcce8
eragaglini <eragaglini7@gmail.com>
2025-06-02 13:18:21 +0200

    removed unnecessary comments

--- arithmetic-evaluator/src/notation_conversion.cpp
+++ arithmetic-evaluator/src/notation_conversion.cpp
@@ -32,11 +32,6 @@ vector<token> infix_to_postfix_helper(vector<token>& tokens, stack<token>& s,
         else if (is_operator(t.c))
         {
             int precedence = get_precedence(t.c);
-            // int last_operator_precedence =
-            //     get_precedence(operators_buffer.back().c);
-            // int first_operator_precedence =
-            //     get_precedence(operators_buffer[0].c);
-
             if (operators_buffer.empty())
             {
                 operators_buffer.push_back(t);
@@ -48,39 +43,25 @@ vector<token> infix_to_postfix_helper(vector<token>& tokens, stack<token>& s,
             }
             else if (precedence <= get_precedence(operators_buffer.back().c))
             {
-                // cout << "riga 51 operator: " << t.c << endl;
                 for (size_t i = 0; i < operators_buffer.size(); i++)
                 {
-                    /* code */
                     s.push(operators_buffer[i]);
                 }
                 operators_buffer.clear();
                 operators_buffer.push_back(t);
-                
             }
             else
             {
-                // cout << "operator: " << t.c << endl;
-
                 auto pos = std::find_if(operators_buffer.begin(),
                                         operators_buffer.end(),
                                         [t](auto s) { return s < t; });
                 operators_buffer.insert(pos, t);
             }
-
-            // cout << "Operators: [";
-            // for (size_t i = 0; i < operators_buffer.size(); i++)
-            // {
-            //     /* code */
-            //     cout << operators_buffer[i].c << ", ";
-            // }
-            // cout << "]" << endl;
         }
         ++it;
     }
     for (auto it = operators_buffer.begin(); it != operators_buffer.end(); ++it)
     {
-        // cout << "operatore: " << it->c << " pushato sullo stack" << endl;
         s.push(*it);
     }
 
