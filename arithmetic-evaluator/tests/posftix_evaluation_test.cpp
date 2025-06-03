df6ded75ee1a0353adbc575e61fe33ac3840ca62
eragaglini <eragaglini7@gmail.com>
2025-06-04 00:36:48 +0200

    added more test cases

--- arithmetic-evaluator/tests/posftix_evaluation_test.cpp
+++ arithmetic-evaluator/tests/posftix_evaluation_test.cpp
@@ -15,3 +15,10 @@ TEST(PostfixEvaluationTest, MultipleParentheses)
     EXPECT_EQ(actual, 53);
 }
 
+TEST(PostfixEvaluationTest, LongExpression)
+{
+    vector<token> tokens = tokenize_expression("(((125*8)-(750/3))+(42*15))/10-((900+150)/50)+((64*4)-(1024/16))*2+(333-111+222)/6-((7*9)+(12*3))*3+((500/25)+(80*5)-30)");
+    double actual = eval_postfix(infix_to_postfix(tokens));
+    EXPECT_EQ(actual, 668);
+}
+
